765864d170c2abcdc2b96df9c268298c6cabb10b
AlessandroMaini <alle.maini2003@gmail.com>
2025-04-21 19:29:05 +0200

    model changed

--- utils/prepare_model.py
+++ utils/prepare_model.py
@@ -1,19 +1,27 @@
+import torch
 import torch.nn as nn
-import timm
 
-# Load the pretrained DINO ViT-S/16 model
-def get_dino_vit_model(num_classes=100, pretrained=True):
-    model = timm.create_model('vit_base_patch16_224', pretrained=pretrained)
-    
-    # Freeze all layers except the final classification layer
-    for param in model.parameters():
-        param.requires_grad = False
-    
-    # Modify the last classification layer to match CIFAR-100's 100 classes
-    model.head = nn.Linear(model.head.in_features, num_classes)
-    
-    # Unfreeze the final classification layer
-    for param in model.head.parameters():
-        param.requires_grad = True
+def get_dino_vit_model(num_classes=100, freeze_backbone=True):
+    """
+    Loads the pretrained DINO ViT-S/16 model from torch.hub and adapts it for CIFAR-100.
+
+    Args:
+        num_classes (int): number of output classes. Default is 100 (CIFAR-100).
+        freeze_backbone (bool): if True, freezes all layers except the classification head.
+
+    Returns:
+        model (torch.nn.Module): the modified DINO ViT-S/16 model.
+    """
+    # Load the pretrained DINO ViT-S/16 model
+    model = torch.hub.load('facebookresearch/dino:main', 'dino_vits16')
+
+    # Freeze the backbone if specified
+    if freeze_backbone:
+        for param in model.parameters():
+            param.requires_grad = False
+
+    # Replace the classification head
+    in_features = model.head.in_features
+    model.head = nn.Linear(in_features, num_classes)
 
     return model
