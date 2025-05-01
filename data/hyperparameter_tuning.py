a894418b404734ae7f659458002b2b912c7a0924
AlessandroMaini <alle.maini2003@gmail.com>
2025-05-01 21:01:54 +0200

    fix

--- data/hyperparameter_tuning.py
+++ data/hyperparameter_tuning.py
@@ -1,10 +1,9 @@
 import torch.optim as optim
 import itertools
-from models.prepare_model import get_frozen_dino_vits16_model
 from eval import evaluate
 from train import train
 
-def run_grid_search(train_loader, val_loader, criterion, device):
+def run_grid_search(train_loader, val_loader, model_fn, criterion, device):
     lr_list = [0.01, 0.001]
     momentum_list = [0.9, 0.95]
     T_max_list = [10, 30]
@@ -13,7 +12,7 @@ def run_grid_search(train_loader, val_loader, criterion, device):
     best_model_state = None
 
     for lr, momentum, T_max in itertools.product(lr_list, momentum_list, T_max_list):
-        model = get_frozen_dino_vits16_model(device)
+        model = model_fn(device)
         optimizer = optim.SGD(model.parameters(), lr=lr, momentum=momentum)
         scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=T_max)
 
