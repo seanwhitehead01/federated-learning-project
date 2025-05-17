096ef4e05c68f6f3aa9b5ac97c5f2c9d5fd0020c
AlessandroMaini <alle.maini2003@gmail.com>
2025-05-17 11:55:45 +0200

    changed-loader

--- data/cifar100_loader.py
+++ data/cifar100_loader.py
@@ -4,20 +4,46 @@
 
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader, random_split
+from torch.utils.data import Dataset
 
-def get_cifar100_loaders(batch_size=256, val_split=0.1):
-    transform = transforms.Compose([
+class TransformedDataset(Dataset):
+    def __init__(self, dataset, transform):
+        self.dataset = dataset
+        self.transform = transform
+
+    def __getitem__(self, index):
+        img, target = self.dataset[index]
+        img = self.transform(img)
+        return img, target
+
+    def __len__(self):
+        return len(self.dataset)
+
+def get_cifar100_loaders(batch_size=64, val_split=0.1):
+    transform_test = transforms.Compose([
         transforms.Resize(224),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.507, 0.487, 0.441], std=[0.267, 0.256, 0.276]),
     ])
 
-    full_train_dataset = datasets.CIFAR100(root='./dataset', train=True, download=True, transform=transform)
+    transform_train = transforms.Compose([
+        transforms.RandomResizedCrop(224, scale=(0.08, 1.0)),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.507, 0.487, 0.441], std=[0.267, 0.256, 0.276]),
+    ])
+
+    # Load dataset without transforms
+    full_train_dataset = datasets.CIFAR100(root='./dataset', train=True, download=True, transform=None)
     val_size = int(len(full_train_dataset) * val_split)
     train_size = len(full_train_dataset) - val_size
-    train_dataset, val_dataset = random_split(full_train_dataset, [train_size, val_size])
+    train_subset, val_subset = random_split(full_train_dataset, [train_size, val_size])
 
-    test_dataset = datasets.CIFAR100(root='./dataset', train=False, download=True, transform=transform)
+    # Wrap subsets with appropriate transforms
+    train_dataset = TransformedDataset(train_subset, transform_train)
+    val_dataset = TransformedDataset(val_subset, transform_test)
+
+    test_dataset = datasets.CIFAR100(root='./dataset', train=False, download=True, transform=transform_test)
 
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True)
     val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=4, pin_memory=True)
